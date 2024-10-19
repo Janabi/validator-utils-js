4ac5ef128e865f06185c3ef91104118c68de3141
Abdulrahman AL-Janabi <abdulrahmanal-janabi@abdulrahmans-macbook-pro.home>
2024-10-19 14:42:23 +0300

    support language in English

--- test.ts
+++ test.ts
@@ -1,8 +1,7 @@
 import { BaseValidation } from "./src";
 
-// const data = BaseValidation.isString().min(10).isDay().validate(
-//   "sunday"
-// );
-const data = BaseValidation.isBoolean().validate('sss');
+const data = BaseValidation.isString().min(10).isDay().validate(
+  "sunday"
+);
 
 console.log(">>>>>>", data);
